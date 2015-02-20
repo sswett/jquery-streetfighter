4c985776aa3bbec8c1e0bd2dfc23d07f46e57814
Steve Swett <sswett@thinkful.com>
2015-02-20 18:01:53 -0500

    make ryu change on mouse events

---
+++ js/app.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+
+ $('.ryu').mouseenter(function() {
+
+	$('.ryu-still').hide();
+	$('.ryu-ready').show();
+
+  })
+
+ .mouseleave(function() {
+
+	$('.ryu-ready').hide();
+	$('.ryu-still').show();  
+})
+
+.mousedown(function() {
+
+	$('.ryu-ready').hide();
+	$('.ryu-throwing').show();
+	$('.hadouken').show();
+
+  })
+
+ .mouseup(function() {
+
+	$('.ryu-throwing').hide();
+	$('.ryu-ready').show();  
+
+});
+
+
+});
